1b255ccc844741188754e59344c05a4505ee2ade
jgawel16 <98536265+jgawel16@users.noreply.github.com>
2025-08-10 16:22:58 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export const SUPABASE_URL = "https://sms-forwarder.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzanR5eXZkanJuc3VmZmppY3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NDM0MjMsImV4cCI6MjA2NzExOTQyM30.pmop0EHxwBbtNO7sFGjsPAEmldXFBj41MBJY5Uxi47o";
+
+// Query de view, niet de tabel
+export const VIEW = "vw_sms_forwarder_public";
+export const COLUMNS = "id,timestamp,body_processed";

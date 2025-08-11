50ea866b88e9eabd28210b2921ddeed70cbf0339
jgawel16 <98536265+jgawel16@users.noreply.github.com>
2025-08-11 11:18:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const SUPABASE_URL = "https://sms-forwarder.supabase.co";
+export const SUPABASE_URL = "https://jsjtyyvdjrnsuffjicqw.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzanR5eXZkanJuc3VmZmppY3F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NDM0MjMsImV4cCI6MjA2NzExOTQyM30.pmop0EHxwBbtNO7sFGjsPAEmldXFBj41MBJY5Uxi47o";
 
 // (Optioneel) auto-refresh in minuten. Zet op 0 om uit te zetten.
